03d29468a3636c85b9882b9ad9318401fa22a97b
Andrew Goodman <goodsoul.andrew@mail.ru>
2025-03-31 13:10:09 +0500

    Можно приблищать и отдалять камеру по колёсику мыши

---
+++ Assets/Scripts/CameraZoomScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OrthographicZoom : MonoBehaviour
+{
+    public Camera cam;
+    private PlayerInput cameraInput;
+    public float maxZoom = 5;
+    public float minZoom = 20;
+    public float sensitivity = 1;
+    public float speed = 30;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cameraInput = GetComponent<PlayerInput>();
+        cameraInput.actions["Zoom"].performed += HandleMouseScroll;
+    }
+
+    private void HandleMouseScroll(InputAction.CallbackContext context)
+    {
+        var input = context.ReadValue<Vector2>();
+        var newZoom = cam.orthographicSize - input.y * sensitivity;
+        //Debug.Log($"newZoom is {newZoom}");
+        newZoom = Mathf.Clamp(newZoom, maxZoom, minZoom);
+        //Debug.Log($"newZoom after clamp is {newZoom}");
+        cam.orthographicSize = newZoom;
+        Debug.Log($"Zoom is {cam.orthographicSize}");
+    }
+}
