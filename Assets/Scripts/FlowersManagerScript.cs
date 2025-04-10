32517a69e2aefa2f7820c0055484cd40b6a2401e
Tim <tim@tim.ru>
2025-04-10 11:43:40 +0500

    чуть рефакторинга

--- Assets/Scripts/FlowersManagerScript.cs
+++ Assets/Scripts/FlowersManagerScript.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class FlowersManagerScript : MonoBehaviour
 {
@@ -11,14 +12,11 @@ public class FlowersManagerScript : MonoBehaviour
     [SerializeField] private int numberOfFlowers;
     [SerializeField] private GameObject Bar;
 
-    private Image[] FlowersImages;
-    private TextMeshProUGUI[] FlowersNumbers;
+    private GameObject[] FlowersNumbers;
     private RectTransform scrollTransform;
 
     private void Start()
     {
-        FlowersImages = new Image[numberOfFlowers];
-        FlowersNumbers = new TextMeshProUGUI[numberOfFlowers];
 
         GameObject scrollObject = new GameObject("scroll");
         
@@ -26,47 +24,13 @@ public class FlowersManagerScript : MonoBehaviour
 
         scrollTransform = scrollObject.AddComponent<RectTransform>();
         scrollTransform.sizeDelta = new Vector2(50, 50);
-        scrollTransform.anchoredPosition = new Vector2((float)(Index - (FlowersObjects.Length - 1) / 2.0)*100, 0);
+        scrollTransform.anchoredPosition = new Vector2((float)(Index - (numberOfFlowers - 1) / 2.0)*100, 0);
 
         Image scrollImage = scrollObject.AddComponent<Image>();
         scrollImage.sprite = scrollSprite;
         scrollObject.layer = LayerMask.NameToLayer("UI");
 
-        for(var i = 0; i < numberOfFlowers; i++)
-        {
-            var xLocation = (float)(i - (FlowersObjects.Length - 1) / 2.0)*100;
-
-            //картинка
-            GameObject imageObject = new GameObject($"NewImage{i}");
-            imageObject.transform.SetParent(Bar.transform,false);
-
-            RectTransform imageTransform = imageObject.AddComponent<RectTransform>();
-            imageTransform.sizeDelta = new Vector2(40, 40);
-            imageTransform.anchoredPosition = new Vector2(xLocation, 0);
-
-            Image image = imageObject.AddComponent<Image>();
-            if(FlowersObjects[i]!=null)
-                image.sprite = FlowersObjects[i].GetComponent<SpriteRenderer>().sprite;
-            FlowersImages[i] = image;
-
-            imageObject.layer = LayerMask.NameToLayer("UI");
-
-            //текст
-            GameObject textObject = new GameObject($"NewText{i}");
-            textObject.transform.SetParent(Bar.transform,false);
-
-            RectTransform textTransform = textObject.AddComponent<RectTransform>();
-            textTransform.sizeDelta = new Vector2(60, 40);
-            textTransform.anchoredPosition = new Vector2(xLocation, -50);
-
-            TextMeshProUGUI text = textObject.AddComponent<TextMeshProUGUI>();
-            if(FlowersCount[i]>=0)
-                text.text = FlowersCount[i].ToString();
-            FlowersNumbers[i]=text;
-            text.alignment = TextAlignmentOptions.Center;
-
-            textObject.layer = LayerMask.NameToLayer("UI");
-        }
+        RecreateFlowersUI();
     }
     public void PlaceFlower(Vector2 position)
     {
@@ -95,8 +59,11 @@ public class FlowersManagerScript : MonoBehaviour
         if (canPlace)
         {
             FlowersCount[Index]--;
-            if(FlowersCount[Index] >= 0)FlowersNumbers[Index].text = FlowersCount[Index].ToString();
-
+            if(FlowersCount[Index] >= 0)
+            {
+                var textComponent = FlowersNumbers[Index].GetComponent<TextMeshProUGUI>();
+                textComponent.text = FlowersCount[Index].ToString();
+            }
             GameObject minion = (GameObject)Instantiate(FlowersObjects[Index], position, Quaternion.Euler(0, 0, 0));
             minion.tag = "PlayerMinion";
             if (minion.TryGetComponent<Plant>(out var plant))
@@ -112,7 +79,68 @@ public class FlowersManagerScript : MonoBehaviour
         {
             Debug.Log($"Новый индекс:{index}");
             Index = index;
-            scrollTransform.anchoredPosition = new Vector2((float)(Index - (FlowersObjects.Length - 1) / 2.0)*100, 0);
+            scrollTransform.anchoredPosition = new Vector2((float)(Index - (numberOfFlowers - 1) / 2.0)*100, 0);
+        }
+    }
+
+    public void SetNumberOfFlowers(int number)
+    {
+        numberOfFlowers = Math.Min(number,FlowersObjects.Length);
+        RecreateFlowersUI();
+        scrollTransform.anchoredPosition = new Vector2((float)(Index - (numberOfFlowers - 1) / 2.0)*100, 0);
+    }
+    public int GetNumberOfFlowers()
+    {
+        return numberOfFlowers;
+    }
+
+    public void RecreateFlowersUI()
+    {
+        if(FlowersNumbers!=null)
+        {
+            foreach (var number in FlowersNumbers)
+            {
+                if (number!=null) Destroy(number);
+            }
+        }
+        FlowersNumbers = new GameObject[numberOfFlowers];
+
+        for(var i = 0; i < numberOfFlowers; i++)
+        {
+            var xLocation = (float)(i - (numberOfFlowers - 1) / 2.0)*100;
+
+            //картинка
+            GameObject imageObject = new GameObject($"NewImage{i}");
+            imageObject.transform.SetParent(Bar.transform,false);
+
+            RectTransform imageTransform = imageObject.AddComponent<RectTransform>();
+            imageTransform.sizeDelta = new Vector2(40, 40);
+            imageTransform.anchoredPosition = new Vector2(xLocation, 0);
+
+            Image image = imageObject.AddComponent<Image>();
+            if(FlowersObjects[i]!=null)
+                image.sprite = FlowersObjects[i].GetComponent<SpriteRenderer>().sprite;
+            imageObject.layer = LayerMask.NameToLayer("UI");
+
+
+
+
+            //текст
+            GameObject textObject = new GameObject($"NewText{i}");
+            textObject.transform.SetParent(Bar.transform,false);
+
+            RectTransform textTransform = textObject.AddComponent<RectTransform>();
+            textTransform.sizeDelta = new Vector2(60, 40);
+            textTransform.anchoredPosition = new Vector2(xLocation, -50);
+
+            TextMeshProUGUI text = textObject.AddComponent<TextMeshProUGUI>();
+            if(FlowersCount[i]>=0)
+                text.text = FlowersCount[i].ToString();
+            text.alignment = TextAlignmentOptions.Center;
+            textObject.layer = LayerMask.NameToLayer("UI");
+
+            FlowersNumbers[i]=textObject;
+
         }
     }
 }
