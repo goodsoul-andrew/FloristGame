82c3ab6fb39e33e306b24aa03fafe35d7bf69d61
Irina Gayntdinova <gajntdinovairina@gmail.com>
2025-05-08 23:06:25 +0500

    Сделала цветочек хилку

---
+++ Assets/Scripts/HealDealer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealDealer : MonoBehaviour
+{
+    public float healAmount;
+    public HashSet<string> Friends = new HashSet<string>();
+    public bool Enabled = true;
+
+    private void Heal(GameObject target)
+    {
+        if (!Enabled) return;
+        if (!Friends.Contains(target.tag)) return;
+        if (target.TryGetComponent<Health>(out var health))
+        {
+            health.Heal(healAmount);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Heal(collision.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Heal(collision.gameObject);
+    }
+}
---
+++ Assets/Scripts/HealPlant.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HealPlant : Plant
+{
+    //private Animator animator;
+    private bool isOpen;
+    [SerializeField] private HealDealer healDealer;
+    [SerializeField] private GameObject hitBox;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        //animator = GetComponent<Animator>();
+        var friends = new string[] { "Player", "PlayerMinion"};
+        healDealer.Friends.AddRange(friends);
+        isOpen = true;
+        //obstacles = new string[] { "Obstacle", "PlayerMinion", "PlantTrap" };
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (healDealer.Friends.Contains(collision.tag))
+        {
+            //Debug.Log($"{collision.tag} {string.Join(", ", damageDealer.Friends)} {damageDealer.Friends.Contains(collision.tag)}");
+            if (isOpen)
+            {
+                isOpen = false;
+                //animator.SetBool("isOpen", false);
+
+                //Debug.Log("Close trap");
+                StartCoroutine(SetActiveAfterDelay(0.1f, true));
+                StartCoroutine(SetActiveAfterDelay(0.25f, false));
+                DestroyAfterDelay(0.8f);
+                //FindFirstObjectByType<TutorialManager>().FinishTutorial("fight");
+            }
+        }
+    }
+
+    public override bool IsAreaAvailable(Vector2 position)
+    {
+        var obstacles = new string[] { "Obstacle", "PlayerMinion", "PlantTrap" };
+        Collider2D[] colliders = GetCollidersInArea(position);
+        //Debug.Log($"{string.Join(", ", obstacles)}");
+        foreach (var collider in colliders)
+        {
+            //Debug.Log($"{collider.tag} {obstacles.Contains(collider.tag)}");
+            if (obstacles.Contains(collider.tag))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private IEnumerator SetActiveAfterDelay(float delay, bool active)
+    {
+        yield return new WaitForSeconds(delay);
+        healDealer.Enabled = active;
+        hitBox.SetActive(active);
+    }
+}
