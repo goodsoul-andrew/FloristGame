c13b9e71e957730e576ba09b596cd4a9919ca688
Andrew Goodman <goodsoul.andrew@mail.ru>
2025-05-12 17:47:17 +0500

    обновил текстуру спавнера

---
+++ Assets/Scripts/QuestFlowerbedScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class QuestFlowerbed : MonoBehaviour
+{
+    [SerializeField] GameObject questPlant;
+    public System.Action OnComplete;
+    private bool isActive;
+
+    void Start()
+    {
+        isActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (! isActive) return;
+        if (questPlant.CompareTag(collision.tag))
+        {
+            OnComplete?.Invoke();
+            Debug.Log("Right plant is planted");
+        }
+    }
+}
