849d969e2af9b7671e0e1a976608253371c454d5
Andrew Goodman <goodsoul.andrew@mail.ru>
2025-05-16 14:19:22 +0500

    Добавил звук могил

--- Assets/Scripts/Quest/QuestFlowerbedScript.cs
+++ Assets/Scripts/Quest/QuestFlowerbedScript.cs
@@ -7,12 +7,14 @@ public class QuestFlowerbed : MonoBehaviour
     public System.Action OnComplete;
     private bool isActive;
     private Health HP;
+    private PlaySoundsScript soundPlayer;
 
     void Start()
     {
         isActive = true;
         HP = GetComponent<Health>();
         HP.IsImmortal = true;
+        soundPlayer = GetComponent<PlaySoundsScript>();
     }
 
     public bool IsRightFlower(QuestFlower flower)
@@ -25,6 +27,7 @@ public class QuestFlowerbed : MonoBehaviour
 
         if (!isActive || !IsRightFlower(flower)) return false;
         OnComplete?.Invoke();
+        soundPlayer.PlaySound(0);
         HP.Kill();
         return true;
     }
