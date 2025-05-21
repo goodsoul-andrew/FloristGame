1696c2a7bc688981ba2612a075cb02fe61a75008
Andrew Goodman <goodsoul.andrew@mail.ru>
2025-05-22 00:23:46 +0500

    корень на TestScene

--- Assets/Scripts/Enemies/RootScript.cs
+++ Assets/Scripts/Enemies/RootScript.cs
@@ -6,18 +6,19 @@ class Root : MonoBehaviour
     [SerializeField] private float attackDelay;
     [SerializeField] private float existenceTime;
     private CircleCollider2D selfCollider;
-    private bool isAttackStarted;
+    private Animator animator;
     private Health hp;
     protected void Awake()
     {
         hp = GetComponent<Health>();
         selfCollider = GetComponent<CircleCollider2D>();
+        animator = GetComponent<Animator>();
     }
 
     protected void Start()
     {
         selfCollider.enabled = false;
-        StartCoroutine(StartAttack());
+        StartCoroutine(Attack());
     }
 
     void Hide()
@@ -25,11 +26,30 @@ class Root : MonoBehaviour
         Destroy(this.gameObject);
     }
 
-    IEnumerator StartAttack()
+    IEnumerator Attack()
     {
         yield return new WaitForSeconds(attackDelay);
-        Debug.Log("Root started attack");
+        // Debug.Log("Root started attack");
+        animator.SetTrigger("StartAttack");
+        yield return new WaitForSeconds(0.4f);
+        //yield return WaitForAnimation("StartAttack");
         selfCollider.enabled = true;
+        yield return new WaitForSeconds(existenceTime);
+        animator.SetTrigger("EndAttack");
+        yield return new WaitForSeconds(0.4f);
+        selfCollider.enabled = false;
+        //yield return WaitForAnimation("EndAttack");
+        Destroy(this.gameObject);
     }
 
+    IEnumerator WaitForAnimation(string animationName)
+    {
+        int animationHash = Animator.StringToHash(animationName);
+
+        yield return new WaitUntil(() =>
+        {
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            return stateInfo.normalizedTime >= 1 && stateInfo.shortNameHash == animationHash;
+        });
+    }
 }
