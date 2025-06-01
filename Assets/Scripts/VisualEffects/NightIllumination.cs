d4e150ab891b74be983f740b9c5ac011a781e78a
Andrew Goodman <goodsoul.andrew@mail.ru>
2025-06-01 20:28:46 +0500

    теперь подсветка включается ночью, а не на закате

--- Assets/Scripts/VisualEffects/NightIllumination.cs
+++ Assets/Scripts/VisualEffects/NightIllumination.cs
@@ -5,6 +5,7 @@ class NightIllumination : MonoBehaviour
 {
     private Daytime daytime;
     private Light2D light2D;
+    [SerializeField] private int numberWhenActivate = 2;
     
     void Start()
     {
@@ -17,7 +18,7 @@ class NightIllumination : MonoBehaviour
             {
                 light2D.enabled = true;
             }
-            daytime.OnDaytimeChange += () => { if (daytime.Time > 1 && light2D != null) light2D.enabled = true; };
+            daytime.OnDaytimeChange += () => { if (daytime.Time > numberWhenActivate && light2D != null) light2D.enabled = true; };
         }
     }   
 }
